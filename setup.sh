8951000a49c33bce05d6c19df84fc649e4321381
Ovidiu Simionica <ovidiu.simionica@gmail.com>
2021-02-06 13:14:05 +0200

    update of docker install

--- setup.sh
+++ setup.sh
@@ -7,13 +7,12 @@ yum -y install git net-tools bind-utils iptables-services bridge-utils bash-comp
 yum -y install https://releases.ansible.com/ansible/rpm/release/epel-7-x86_64/ansible-2.6.6-1.el7.ans.noarch.rpm
 
 
-yum -y install wget curl java-1.8.0-openjdk-devel nano emacs dnsmasq docker
-usermod -aG dockerroot vagrant
-cat > /etc/docker/daemon.json <<EOF
-{
-    "group": "dockerroot"
-}
-EOF
+yum -y install wget curl java-1.8.0-openjdk-devel nano emacs dnsmasq
+
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install docker-ce-19.03.15-3.el7 docker-ce-cli-19.03.15-3.el7 containerd.io
+
+usermod -aG docker vagrant
 systemctl enable docker
 systemctl start docker
 
